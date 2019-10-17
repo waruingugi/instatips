6b64e334f02ce1d086056e0af8c0fa584ea31e06
Warui Ngugi <waruingugientp@gmail.com>
2019-10-17 20:15:09 +0300

    Added celery scheduler (manual)

---
+++ instatips/celery.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instatips.settings')
+
+app = Celery('instatips')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+app.conf.beat_schedule = {
+    #name of the scheduler
+
+    'add-every-2-seconds': {
+        # task name which we have created in tasks.py
+
+        'task': 'add_2_numbers',  
+        # set the period of running
+        
+        'schedule': 2.0,
+         # set the args 
+         
+        'args': (16, 16) 
+    },
+    #name of the scheduler
+
+    'print-name-every-5-seconds': {  
+        # task name which we have created in tasks.py
+
+        'task': 'print_msg_with_name',  
+        
+        # set the period of running
+
+        'schedule': 5.0,  
+        # set the args
+
+       'args': ("DjangoPY", )  
+    },
+}
