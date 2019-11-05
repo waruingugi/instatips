898c6a00f8ebc8afc962f6813c8a117008431cea
Warui Ngugi <waruingugientp@gmail.com>
2019-11-05 23:21:19 +0300

    Changed white noise version back to version 4

--- instatips/settings.py
+++ instatips/settings.py
@@ -183,3 +183,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url  # noqa
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+WHITENOISE_USE_FINDERS = True
